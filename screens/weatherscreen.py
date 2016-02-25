5b2cc5c5dfbf8ed0edcfd641cf18ac64b6ee10c2
Kevin Kahn <kevin@coral-zone.com>
2016-02-24 16:32:03 -0800

    Split weather fetching from weather screen to allow other uses of weather info

--- screens/weatherscreen.py
+++ screens/weatherscreen.py
@@ -12,61 +12,11 @@ import urllib2
 import json
 import logsupport
 import functools
+import weatherinfo
 import utilities
 from utilities import scaleW, scaleH
 
 fsizes = ((scaleH(20), False, False), (scaleH(30), True, False), (scaleH(45), True, True))  # todo pixel
-
-
-def TreeDict(d, *args):
-	# Allow a nest of dictionaries to be accessed by a tuple of keys for easier code
-	if len(args) == 1:
-		temp = d[args[0]]
-		if isinstance(temp, basestring) and temp.isdigit():
-			temp = int(temp)
-		else:
-			try:
-				temp = float(temp)
-			except (ValueError, TypeError):
-				pass
-
-		return temp
-	else:
-		return TreeDict(d[args[0]], *args[1:])
-
-
-def RenderScreenLines(recipe, extracter, color):
-	h = 0
-	renderedlines = []
-	centered = []
-	linetorender = ""
-	for line in recipe:
-		linestr = ""
-		try:
-			if isinstance(line[3], basestring):
-				linestr = line[2].format(d=line[3])
-			else:
-				args = []
-				for item in line[3]:
-					args.append(extracter(*item))
-				if len(args) == 1 and isinstance(args[0], basestring):
-					linestr = line[2].format(d=args[0])
-				else:
-					linestr = line[2].format(d=args)
-		except:
-			config.Logs.Log("Weather format error: " + str(line[3]), logsupport.Warning)
-			if len(line) == 5:
-				linestr = line[4]
-		if line[1] == 2:
-			linetorender = linetorender + linestr
-		else:
-			r = config.fonts.Font(fsizes[line[0]][0], '', fsizes[line[0]][1], fsizes[line[0]][2]).render(
-				linetorender + linestr, 0, wc(color))
-			linetorender = ""
-			renderedlines.append(r)
-			centered.append(line[1])
-			h = h + r.get_height()
-	return renderedlines, centered, h
 
 
 class WeatherScreenDesc(screen.ScreenDesc):
@@ -75,62 +25,77 @@ class WeatherScreenDesc(screen.ScreenDesc):
 
 		screen.ScreenDesc.__init__(self, screensection, screenname, ('which',))
 		utilities.LocalizeParams(self, screensection, WunderKey='', location='')
-		self.lastwebreq = 0  # time of last call out to wunderground
-		self.url = 'http://api.wunderground.com/api/' + self.WunderKey + '/geolookup/conditions/forecast/astronomy/q/' + self.location + '.json'
-		self.parsed_json = {}
-		self.scrlabel = ""
-		for s in self.label:
-			self.scrlabel = self.scrlabel + " " + s
-		self.js = None  # partial function for dict access
-		self.fcsts = None  # partial function for dict access
-		# entries are (fontsize, centered, formatstring, jsonextractstring,altstring(optional))
+		self.scrlabel = screen.FlatenScreenLabel(self.label)
+		# entries are (fontsize, centered, formatstring, values)
 		self.conditions = [(2, True, "{d}", self.scrlabel),
-						   (1, True, "{d}", (('location', 'city'),)),
-						   (1, False, u"Currently: {d[0]} {d[1]}\u00B0F",
-							(('current_observation', 'weather'), ('current_observation', 'temp_f'))),
-						   (0, False, u"  Feels like: {d[0]}\u00B0", (('current_observation', 'feelslike_f'),)),
-						   (1, False, "Wind {d[0]} at {d[1]} gusts {d[2]}", (
-							   ('current_observation', 'wind_dir'), ('current_observation', 'wind_mph'),
-							   ('current_observation', 'wind_gust_mph'))),
-						   (1, False, "Sunrise: {d[0]:02d}:{d[1]:02d}",
-							(('sun_phase', 'sunrise', 'hour'), ('sun_phase', 'sunrise', 'minute'))),
-						   (1, False, "Sunset:  {d[0]:02d}:{d[1]:02d}",
-							(('sun_phase', 'sunset', 'hour'), ('sun_phase', 'sunset', 'minute'))),
-						   (0, 2, "Moon rise: {d[0]:02d}:{d[1]:02d}",
-							(('moon_phase', 'moonrise', 'hour'), ('moon_phase', 'moonrise', 'minute')), 'No moonrise'),
-						   (0, False, "   set: {d[0]:02d}:{d[1]:02d}",
-							(('moon_phase', 'moonset', 'hour'), ('moon_phase', 'moonset', 'minute')), '   No moonset'),
-						   (0, False, "     {d[0]}% illuminated", (('moon_phase', 'percentIlluminated'),)),
+						   (1, True, "{d[0]}", ('Location',)),
+						   (1, False, u"Now: {d[0]} {d[1]}\u00B0F", ('Sky', 'Temp')),
+						   (0, False, u"  Feels like: {d[0]}\u00B0", ('Feels',)),
+						   (1, False, "Wind {d[0]} at {d[1]} gusts {d[2]}", ('WindDir', 'WindMPH', 'WindGust')),
+						   (1, False, "Sunrise: {d[0]:02d}:{d[1]:02d}", ('SunriseH', 'SunriseM')),
+						   (1, False, "Sunset:  {d[0]:02d}:{d[1]:02d}", ('SunsetH', 'SunsetM')),
+						   (0, 2, "Moon rise: {d[0]:02d}:{d[1]:02d}", ('MoonriseH', 'MoonriseM')),
+						   (0, False, "   set: {d[0]:02d}:{d[1]:02d}", ('MoonsetH', 'MoonsetM')),
+						   (0, False, "     {d[0]}% illuminated", ('MoonPct',)),
 						   (0, False, "will be replaced", "")]
-		self.forecast = [(1, False, u"{d[0]}   {d[1]}\u00B0/{d[2]}\u00B0 {d[3]}",
-						  (('date', 'weekday_short'), ('high', 'fahrenheit'), ('low', 'fahrenheit'), ('conditions',))),
-						 (1, False, "Wind: {d[0]} at {d[1]}", (('avewind', 'dir'), ('avewind', 'mph')))]
+		self.forecast = [(1, False, u"{d[0]}   {d[1]}\u00B0/{d[2]}\u00B0 {d[3]}", ('Day', 'High', 'Low', 'Sky')),
+						 (1, False, "Wind: {d[0]} at {d[1]}", ('WindDir', 'WindSpd'))]
+		self.Info = weatherinfo.WeatherInfo(self.WunderKey, self.location)
 		utilities.register_example("WeatherScreenDesc", self)
 
 	def __repr__(self):
 		return screen.ScreenDesc.__repr__(self) + "\r\n     WeatherScreenDesc:" + str(self.CharColor)
 
+	def RenderScreenLines(self, recipe, values, color):
+		h = 0
+		renderedlines = []
+		centered = []
+		linetorender = ""
+		for line in recipe:
+			try:
+				if isinstance(line[3], basestring):  # handles case of a direct var e.g. label string
+					linestr = line[2].format(d=line[3])
+				else:
+					args = []
+					for item in line[3]:
+						args.append(values[item])
+					linestr = line[2].format(d=args)
+			except:
+				config.Logs.Log("Weather format error: " + str(line[3]), logsupport.Warning)
+				linestr = 'error in feed'
+			if line[
+				1] == 2:  # todo this can go away if I'n not going to provide an alternate value display (no moonrise e.g.)
+				linetorender = linetorender + linestr
+			else:
+				r = config.fonts.Font(fsizes[line[0]][0], '', fsizes[line[0]][1], fsizes[line[0]][2]).render(
+					linetorender + linestr, 0, wc(color))
+				linetorender = ""
+				renderedlines.append(r)
+				centered.append(line[1])  # todo this needed only because of the concatenate option (2) above
+				h = h + r.get_height()
+		return renderedlines, centered, h
+
+
+
 	def ShowScreen(self, conditions):
 		self.SetExtraCmdTitles(([('Conditions',)], [('Forecast',)])[conditions])
 		self.PaintBase()
 		usefulheight = config.screenheight - config.topborder - config.botborder
-		renderedlines = []
 		h = 0
 		centered = []
-
+		self.Info.FetchWeather()
 		if conditions:
-			age = utilities.interval_str(time.time() - int(self.js('current_observation', 'observation_epoch')))
+			age = utilities.interval_str(time.time() - self.Info.ConditionVals['Time'])
 			self.conditions[-1] = (0, False, "Readings as of {d} ago", age)
-			renderedlines, centered, h = RenderScreenLines(self.conditions, self.js, self.CharColor)
+			renderedlines, centered, h = self.RenderScreenLines(self.conditions, self.Info.ConditionVals,
+																self.CharColor)
 		else:
-			renderedlines.append(
-				config.fonts.Font(fsizes[2][0], '', fsizes[2][1], fsizes[2][2]).render(self.scrlabel, 0,
-																					   wc(self.CharColor)))
+			renderedlines = [config.fonts.Font(fsizes[2][0], '', fsizes[2][1], fsizes[2][2]).render(self.scrlabel, 0,
+																									wc(self.CharColor))]
 			centered.append(True)
 			h = h + renderedlines[0].get_height()
-			for fcst in self.fcsts:
-				fs = functools.partial(TreeDict, fcst)
-				r, c, temph = RenderScreenLines(self.forecast, fs, self.CharColor)
+			for fcst in self.Info.ForecastVals:
+				r, c, temph = self.RenderScreenLines(self.forecast, fcst, self.CharColor)
 				h += temph
 				renderedlines = renderedlines + r
 				centered = centered + c
@@ -151,24 +116,8 @@ class WeatherScreenDesc(screen.ScreenDesc):
 
 		# stop any watching for device stream
 		config.toDaemon.put([])
-
 		currentconditions = True
-
-		if time.time() > self.lastwebreq + 5*60:
-			# refresh the conditions - don't do more than once per 5 minutes
-			f = urllib2.urlopen(self.url)
-			val = f.read()
-			if val.find("keynotfound") <> -1:
-				config.Logs.Log("Bad weatherunderground key:" + self.name, severity=logsupport.Error)
-				return config.HomeScreen
-			self.lastwebreq = time.time()
-			self.parsed_json = json.loads(val)
-
-			self.js = functools.partial(TreeDict, self.parsed_json)
-			self.fcsts = TreeDict(self.parsed_json, 'forecast', 'simpleforecast', 'forecastday')
-			f.close()
 		self.ShowScreen(currentconditions)
-
 		while 1:
 			choice = config.DS.NewWaitPress(self)
 			if choice[0] == WAITEXIT:
---
+++ weatherinfo.py
@@ -0,0 +1,81 @@
+import functools
+import json
+import time
+import urllib2
+
+import config
+import logsupport
+
+
+def TreeDict(d, *args):
+	# Allow a nest of dictionaries to be accessed by a tuple of keys for easier code
+	if len(args) == 1:
+		temp = d[args[0]]
+		if isinstance(temp,
+					  basestring) and temp.isdigit():  # todo now that I spec type above is this stuff still needed
+			temp = int(temp)
+		else:
+			try:
+				temp = float(temp)
+			except (ValueError, TypeError):
+				pass
+
+		return temp
+	else:
+		return TreeDict(d[args[0]], *args[1:])
+
+
+class WeatherInfo:
+	ConditionMap = {'Time': (int, ('current_observation', 'observation_epoch')),
+					'Location': (str, ('location', 'city')),
+					'Temp': (float, ('current_observation', 'temp_f')),
+					'Sky': (str, ('current_observation', 'weather')),
+					'Feels': (float, ('current_observation', 'feelslike_f')),
+					'WindDir': (str, ('current_observation', 'wind_dir')),
+					'WindMPH': (float, ('current_observation', 'wind_mph')),
+					'WindGust': (float, ('current_observation', 'wind_gust_mph')),
+					'SunriseH': (int, ('sun_phase', 'sunrise', 'hour')),
+					'SunriseM': (int, ('sun_phase', 'sunrise', 'minute')),
+					'SunsetH': (int, ('sun_phase', 'sunset', 'hour')),
+					'SunsetM': (int, ('sun_phase', 'sunset', 'minute')),
+					'MoonriseH': (int, ('moon_phase', 'moonrise', 'hour')),  # intstr
+					'MoonriseM': (int, ('moon_phase', 'moonrise', 'minute')),
+					'MoonsetH': (int, ('moon_phase', 'moonset', 'hour')),
+					'MoonsetM': (int, ('moon_phase', 'moonset', 'minute')),
+					'MoonPct': (int, ('moon_phase', 'percentIlluminated')),
+					}
+	ForecastDay = {'Day': (str, ('date', 'weekday_short')),
+				   'High': (int, ('high', 'fahrenheit')),
+				   'Low': (int, ('low', 'fahrenheit')),
+				   'Sky': (str, ('conditions',)),
+				   'WindDir': (str, ('avewind', 'dir')),
+				   'WindSpd': (float, ('avewind', 'mph'))}
+
+	def __init__(self, WunderKey, location):
+		self.lastwebreq = 0  # time of last call out to wunderground
+		self.url = 'http://api.wunderground.com/api/' + WunderKey + '/geolookup/conditions/forecast/astronomy/q/' \
+				   + location + '.json'
+		self.ConditionVals = {}
+		self.ForecastVals = []
+
+	def FetchWeather(self):
+		if time.time() > self.lastwebreq + 5*60:
+			# refresh the conditions - don't do more than once per 5 minutes
+			f = urllib2.urlopen(self.url)
+			val = f.read()
+			if val.find("keynotfound") <> -1:
+				config.Logs.Log("Bad weatherunderground key:" + self.name, severity=logsupport.Error)
+				return config.HomeScreen
+			self.lastwebreq = time.time()
+			parsed_json = json.loads(val)
+			js = functools.partial(TreeDict, parsed_json)
+			fcsts = TreeDict(parsed_json, 'forecast', 'simpleforecast', 'forecastday')
+			f.close()
+			for cond, desc in WeatherInfo.ConditionMap.iteritems():
+				self.ConditionVals[cond] = desc[0](js(*desc[1]))
+			for i, fcst in enumerate(fcsts):
+				self.ForecastVals.append({})
+				fs = functools.partial(TreeDict, fcst)
+				for fc, desc in WeatherInfo.ForecastDay.iteritems():
+					self.ForecastVals[i][fc] = desc[0](fs(*desc[1]))
+		return self.lastwebreq
