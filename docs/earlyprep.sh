10071e2cf6946cb603a62f8c37b7ac8bc9c82856
Kevin Kahn <kevin@coral-zone.com>
2016-10-10 15:30:18 -0700

    inital pgaw version

--- docs/earlyprep.sh
+++ docs/earlyprep.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
 # Meant to be put on boot file system when SD card is created then moved and run from pi home dir
-sudo passwd pi
+#sudo passwd pi
 #sudo raspi-config
 echo "Make sure to set up network"
+cd /home/pi/.config/lxterminal
 echo "
 /fontname/c \\
 fontname = Monospace Bold 13
@@ -12,6 +13,7 @@ bgcolor=#5ddb1a55f009
 /fgcolor/c \\
 fgcolor=#c63eef9a0c11
 " > lxfix
-cp /home/pi/.config/lxterminal/lxterminal.conf /home/pi/lxterminal.conf.bak
-sed -f lxfix /home/pi/.config/lxterminal/lxterminal.conf.bak > /home/pi/.config/lxterminal/lxterinal.conf2
+cp lxterminal.conf lxterminal.conf.bak
+sed -f lxfix lxterminal.conf.bak > lxterinal.conf2
+cd
 wget https://raw.githubusercontent.com/kevinkahn/softconsole/master/docs/piprep.sh
