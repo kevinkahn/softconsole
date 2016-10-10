9ad11bfb7fb6560dbbdaf6ee7913812bd66f56ed
Kevin Kahn <kevin@coral-zone.com>
2016-10-10 15:23:30 -0700

    inital pgaw version

--- docs/earlyprep.sh
+++ docs/earlyprep.sh
@@ -5,12 +5,13 @@ sudo passwd pi
 #sudo raspi-config
 echo "Make sure to set up network"
 echo "
-/fontname/c \
+/fontname/c \\
 fontname = Monospace Bold 13
-/bgcolor/c \
+/bgcolor/c \\
 bgcolor=#5ddb1a55f009
-/fgcolor/c \
+/fgcolor/c \\
 fgcolor=#c63eef9a0c11
 " > lxfix
-sed -f lxfix /home/pi/.config/lxterminal/lxterminal.conf.bak /home/pi/.config/lxterminal/lexterinal.conf
+cp /home/pi/.config/lxterminal/lxterminal.conf /home/pi/lxterminal.conf.bak
+sed -f lxfix /home/pi/.config/lxterminal/lxterminal.conf.bak > /home/pi/.config/lxterminal/lxterinal.conf2
 wget https://raw.githubusercontent.com/kevinkahn/softconsole/master/docs/piprep.sh
