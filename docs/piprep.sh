35942635e3deb3e631e7ae64b92c84fe430bf9ef
Kevin Kahn <kevin@coral-zone.com>
2016-11-25 20:00:35 -0800

    Merge remote-tracking branch 'origin/master' into NewCoreStructure
    # Conflicts:
    #	docs/piprep.sh

--- docs/piprep.sh
+++ docs/piprep.sh
@@ -235,6 +235,7 @@ mv /boot/config.txt /boot/config.sav
 sed s/rotate=90/rotate=0/ /boot/config.sav > /boot/config.txt
 python /home/pi/adafruit-pitft-touch-cal -f -t $ScreenType -r 0
 
+
 cd /home/pi/
 LogBanner "Console Installation"
 echo "-------Install Console-------" >> /home/pi/log.txt
