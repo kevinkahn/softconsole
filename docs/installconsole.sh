eb7427d1a2e1f7e4c1a6b6476233be595b5a5ad5
Kevin Kahn <kevin@coral-zone.com>
2017-09-18 14:28:20 -0700

    Allow params to come from command line for auto running from pisetup

--- docs/installconsole.sh
+++ docs/installconsole.sh
@@ -48,18 +48,35 @@ then
   exit
 fi
 
-Get_yn Personal "Is this the developer personal system (Y/N) (risky to say Y if it not)?"
-Get_yn AutoConsole "Autostart console (Y/N)?"
+# script can take 2 parameters to preanswer Personal and AutoConsole
+# this supports autorunning at reboot
+SkipVerify=Y
+if [ -n "$1" ]
+then
+  Personal=$1
+else
+  Get_yn Personal "Is this the developer personal system (Y/N) (risky to say Y if it not)?"
+  SkipVerify=N
+fi
+if [ -n "$2" ]
+then
+  AutoConsole=$2
+else
+  Get_yn AutoConsole "Autostart console (Y/N)?"
+  SkipVerify=N
+fi
 
 echo "Developer system:           $Personal"
 echo "Auto start Console on boot: $AutoConsole"
 
-Get_yn Go "Proceed?"
-if [ "$Go" != "Y" ]
+if [ "$SkipVerify" != "Y" ]
 then
-  exit 1
+    Get_yn Go "Proceed?"
+    if [ "$Go" != "Y" ]
+    then
+      exit 1
+    fi
 fi
-
 
 date >> /home/pi/prep.log
 echo "Developer system:           $Personal" >> /home/pi/prep.log
@@ -139,10 +156,9 @@ fi
 
 LogBanner "Install and setup finished"
 LogBanner "Rebooting in 5 seconds"
-i=5
 for i in 5 4 3 2 1
 do
-  echo Rebooting $1
+  echo Rebooting $i
   sleep 1
 done
 echo "Reboot . . ."
