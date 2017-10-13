555e45ff863e252fd21975b4f1a381d329896893
Kevin Kahn <kevin@coral-zone.com>
2017-10-12 18:14:54 -0700

    More work on value change screen

--- valuechangescreen.py
+++ valuechangescreen.py
@@ -1,28 +1,11 @@
-import pygame
 import screen
 import config
 import webcolors as wc
-import utilities
+from utilities import wc
 import functools
+import pygame
 from pygame import draw
 from toucharea import TouchPoint, ManualKeyDesc
-
-
-def ShowVarChangeScreen(varid, varname, initvalue):
-	pass
-
-
-"""
-proc to draw triangle with number in middle, up/down with underlying touch area
-start with center of triangle; compute the 3 vertices; do a draw aalines using them; need to scale for screen w/h
-
-arrowsize = scaleH(40)
-self.Background = KeyColorOn
-self.Outline = KeyOnOutlineColor
-self.CharColor = KeyCharColorOn
-pygame.draw.aalines(Surf,Color,True,[[y+arrowsize/2,x], [y - arrowsize/2,x+arrowsize/2], [y - arrowsize/2,x-arrowsize/2]]
-"""
-
 
 class ValueChangeScreen(screen.ScreenDesc):
 	# need to set no nav keys
@@ -37,66 +20,67 @@ class ValueChangeScreen(screen.ScreenDesc):
 		pass
 
 	def ValChange(self, delta):
+		pass
 
 	def __init__(self, Background, Outline, CharColor, label, initvalue, changevals, setvalueproc, returnscreen):
 		self.__dict__.update({k: v for k, v in locals().items() if k != 'self'})
+		self.Value = initvalue
 		self.Keys = {}
 		vertzonepct = .8
 		vertzonesize = int(.25*config.screenheight)
 		screencenter = (config.screenheight/2, config.screenwidth/2)
 
-		valuebuttonsize = (config.screenwidth/2, int(vertzonesize*vertzonepct))
-		valuebuttoncenter = screencenter
-
 		arrowhalfsize = int(vertzonesize*vertzonepct/2)
-		uparrowcenter = []
-		dnarrowcenter = []
+		self.uparrowcenter = []
+		self.dnarrowcenter = []
+		self.uparrow = [[arrowhalfsize, 0], [-arrowhalfsize, arrowhalfsize],
+						[-arrowhalfsize, -arrowhalfsize]]  # verticies ref 0,0 center
+		self.dnarrow = [[-arrowhalfsize, 0], [arrowhalfsize, arrowhalfsize], [arrowhalfsize, -arrowhalfsize]]
+
 		for i in range(len(changevals)):
-			uparrowcenter.append((screencenter[0] + vertzonesize, (i + 1)*config.screenwidth/(len(changevals) + 1)))
-			self.Keys['up' + str(i)] = TouchPoint('up' + str(i), uparrowcenter[
+			self.uparrowcenter.append(
+				(screencenter[0] + vertzonesize, (i + 1)*config.screenwidth/(len(changevals) + 1)))
+			self.Keys['up' + str(i)] = TouchPoint('up' + str(i), self.uparrowcenter[
 				-11, 2*arrowhalfsize, functools.partial(self.ValChange, changevals[i])])
-			dnarrowcenter.append((screencenter[0] - vertzonesize, (i + 1)*config.screenwidth/(len(changevals) + 1)))
+			self.uparrowverts = \
+				[map(functools.partial(self.offsetpoint, self.uparrowcenter[-1]), self.uparrow[k]) for k in range(3)]
+			self.dnarrowcenter.append(
+				(screencenter[0] - vertzonesize, (i + 1)*config.screenwidth/(len(changevals) + 1)))
 			self.Keys['dn' + str(i)] = TouchPoint('up' + str(i),
-												  uparrowcenter[-11, 2*arrowhalfsize, functools.partial(self.ValChange,
-																										-changevals[
-																											i])])
-		# create pointlist for verticies
+												  self.dnarrowcenter[
+													  -11, 2*arrowhalfsize, functools.partial(self.ValChange,
+																							  -changevals[i])])
+			self.dnarrowverts = \
+				[map(functools.partial(self.offsetpoint, self.dnarrowcenter[-1]), self.dnarrow[k]) for k in range(3)]
 
-		uparrow = [[arrowhalfsize, 0], [-arrowhalfsize, arrowhalfsize],
-				   [-arrowhalfsize, -arrowhalfsize]]  # verticies ref 0,0 center
-		dnarrow = [[-arrowhalfsize, 0], [arrowhalfsize, arrowhalfsize], [arrowhalfsize, -arrowhalfsize]]
-
-		titlecenter = (screencenter[0] - int(1.75*vertzonesize), screencenter[1])
+		self.titlecenter = (screencenter[0] - int(1.75*vertzonesize), screencenter[1])
+		valuebuttoncenter = screencenter
+		valuebuttonsize = (config.screenwidth/2, int(vertzonesize*vertzonepct))
 		cancelcenter = (screencenter[0] + int(1.75*vertzonesize), screencenter[1])
 		cancelsize = (int(vertzonepct*config.screenheight*.125), config.screenwidth/2)
 
 		self.Keys['cancel'] = ManualKeyDesc(self, 'cancel', 'Cancel', Background, CharColor, CharColor, cancelcenter,
 											cancelsize, Proc=self.CancelChange)
-		self.Keys['accept'] = ManualKeyDesc(self, 'accept', label, Background, CharColor, CharColor, valuebuttoncenter,
+		self.Keys['accept'] = ManualKeyDesc(self, 'accept', ["Accept", "#"], Background, CharColor, CharColor,
+											valuebuttoncenter,
 											valuebuttonsize, self.AcceptChange)
 		# need to insert current value (actually in PaintKey probably
 		pass
 
 	def InitDisplay(self, nav):
 		super(ValueChangeScreen, self).InitDisplay(nav)
-		# need to write title, need to put value in accept key, need to draw arrows
+
 		config.screen.fill(wc(self.Background))
-		pygame.draw.lines(Surf, self.Outline, True, pointlist, width=2)
+		# write the title with name of var? maybe button should be "accept"
+		for i in range(len(self.changevals)):
+			draw.lines(config.screen, self.Outline, True, self.uparrowverts, width=2)
+			draw.lines(config.screen, self.Outline, True, self.dnarrowverts, width=2)
 		# need to add in the value to change by or make this an aalines call
+		self.Keys['accept'].SetKeyImages(("Accept", str(self.Value)))
 		self.PaintKeys()
 		pygame.display.update()
 
 
-"""
-		pygame.draw.lines(Surf, self.Outline, True, map(functools.partial(self.offsetpoint,center),uparrow), width = 2)
-		# want this to really have a param that is center so can use in a loop easily
-
-		new_list = [x + 1 for x in my_list]
-		[sum(x) for x in zip(center, p in uparrow)]
-		[5, 7, 9]
-		
-		map(functools.partial(offsetpoint, center), uparrow)
-"""
 
 print "start"
 config.screenwidth = 320
