88377b4432542d61df573c3a551e3a891511a115
kevinkahn <kevin@coral-zone.com>
2018-02-07 20:42:02 -0800

    Bug fixes getting to systemd

--- scripts/upgradeprep.sh
+++ scripts/upgradeprep.sh
@@ -3,5 +3,12 @@ pip install webcolors
 pip install xmltodict
 pip install websocket-client
 pip install wiringpi
-python ../setupsystemd.py
-# this runs in previous version so the new python code is up a level
+if [ -e setupsystemd.py ]
+then
+    echo Initial install
+    python setupsystemd.py
+else
+    echo Upgrade process
+    python ../setupsystemd.py
+    # this runs in previous version so the new python code is up a level
+fi
