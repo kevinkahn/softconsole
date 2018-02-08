15b9f126614d1d72320270be728727472989b023
kevinkahn <kevin@coral-zone.com>
2018-02-07 20:16:00 -0800

    Bug fixes getting to systemd

--- setupsystemd.py
+++ setupsystemd.py
@@ -1,7 +1,7 @@
 import subprocess, os
 
 print('Setup systemd')
-os.chmod('runconsole.py', 0o555)
-os.chmod('console.py',0o555)
-subprocess.call('cp -f scripts/softconsole.service /usr/lib/systemd/system', shell=True)
+os.chmod('/home/pi/consolestable/runconsole.py', 0o555)
+os.chmod('/home/pi/consolestable/console.py',0o555)
+subprocess.call('cp -f /home/pi/consolestable/scripts/softconsole.service /usr/lib/systemd/system', shell=True)
 subprocess.call('systemctl daemon-reload', shell=True)
