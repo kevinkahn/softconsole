7471ed374931f53742f033abaf9abc66304f4f54
kevinkahn <kevin@coral-zone.com>
2018-02-27 19:54:19 -0800

    Add ISYDump capability Guard against exceptions in debugPrint just in case

--- debug.py
+++ debug.py
@@ -1,24 +1,30 @@
 import config
 import sys
-from logsupport import ConsoleDebug
+import mypprint
+
+from logsupport import ConsoleDebug, ConsoleError
 
 Flags = {}
 DbgFlags = ['Main', 'DaemonCtl', 'DaemonStream', 'Screen', 'ISY', 'Dispatch', 'EventList', 'Fonts', 'DebugSpecial',
-			'QDump','LLTouch','Touch']
+			'QDump', 'LLTouch', 'Touch', 'ISYDump']
 DebugFlagKeys = {}
 flagspercol = 3  # number of flags per maint screen
 flagsperrow = 2
+
 
 def debugPrint(flag, *args):
 	global Flags, DbgFlags
 	if flag in DbgFlags:
 		if Flags[flag]:
-			print >> sys.stderr, flag, '-> ',
-			for arg in args:
-				print >> sys.stderr, arg,
-			print >> sys.stderr
-			if config.Logs is not None:
-				config.Logs.Log(flag, '-> ', *args, severity=ConsoleDebug, diskonly=True)
+			try:
+				print >> sys.stderr, flag, '-> ',
+				for arg in args:
+					print >> sys.stderr, arg,
+				print >> sys.stderr
+				if config.Logs is not None:
+					config.Logs.Log(flag, '-> ', *args, severity=ConsoleDebug, diskonly=True)
+			except:
+				config.Logs.Log("Internal debug print error: ", flag, ' ', repr(args), severity=ConsoleError)
 	else:
 		print >> sys.stderr, "DEBUG FLAG NAME ERROR", flag
 
@@ -32,3 +38,15 @@ def InitFlags():
 		except:
 			dbg[flg] = False
 	return dbg
+
+def ISYDump(fn, item, pretty = True,new=False):
+	fm = 'w' if new else 'a'
+	with open('/home/pi/Console/'+fn,mode=fm) as f:
+		if pretty:
+			if not new:
+				f.write('\n--------------------------------------------------------\n')
+			mypprint.pprint(item,stream=f,indent=3,width=50)
+		else:
+			if not new:
+				f.write('\n')
+			f.write(item)
