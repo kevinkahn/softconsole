ebfba219a9d9c6f69c124ee94c07f59a0da03f1f
kevinkahn <kevin@coral-zone.com>
2018-04-21 18:34:05 -0700

    Have minimal example use new hub config section and get a name

--- getsetupinfo.py
+++ getsetupinfo.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from future.builtins.misc import input
 
+ISYname = ""
 ans = ""
 ISYIP = ""
 ISYUSER = ""
@@ -11,27 +12,33 @@ while not ans in ('y', 'Y', 'n', 'N'):
 if ans in ('y', 'Y'):
 	go = ""
 	while not go in ('y', 'Y'):
+		ISYname = input("Name to use for the ISY hub (defaults to ISY): ")
+		if ISYname == "":
+			ISYname = "ISY"
 		ISYIP = input("full URL to access ISY: ")
 		if ISYIP.endswith('/'):
 			ISYIP = ISYIP[0:-1]
 		ISYUSER = input("ISY user name: ")
 		ISYPWD = input("ISY password: ")
 		exswitch = input("Example switch to use (ISY name): ")
+		print("Name:    " + ISYname)
 		print("IP:      " + ISYIP)
 		print("USER:    " + ISYUSER)
 		print("PASSWORD:" + ISYPWD)
 		print("SWITCH:  " + "[[" + exswitch + "]]")
 		go = input("OK? (y/n)")
 	with open('/home/pi/Consoleauth', "w") as f:
-		cfg = ("ISYaddr = " + ISYIP,
-			   "ISYuser = " + ISYUSER,
-			   "ISYpassword = " + ISYPWD,
-			   "WunderKey = xxx",
+		cfg = ("[" + ISYname + "]",
+			   "type = ISY",
+			   "address = " + ISYIP,
+			   "user = " + ISYUSER,
+			   "password = " + ISYPWD,
 			   "\n")
 		f.write("\n".join(cfg))
 	with open('/home/pi/ConsoleMinEx', 'w') as f:
 		cfg = ('cfglib = cfglib',
 			   'include = auth.cfg, myclock.cfg',
+			   'DefaultHub = ISY',
 			   'HomeScreenName = test',
 			   'PersistTO = 30',
 			   'DimLevel = 5',
