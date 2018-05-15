482673d27e5e8368922ecb9971d045191653b904
Kevin Kahn <kevin@coral-zone.com>
2018-05-15 14:17:10 -0700

    Font issues

--- fonts.py
+++ fonts.py
@@ -19,26 +19,27 @@ class Fonts(object):
 	# initialize with 1 font for use in early abort messages (40,"",True,True)
 
 	def Font(self, fsize, face="", bold=False, italic=False):
-        def gennewfont(name, size, bold, italic):
+		def gennewfont(name, size, bold, italic):
+			logsupport.Logs.Log('Generated Font: ', repr(name), str(size), str(utilities.scaleH(size)), str(bold),
+								str(italic))
 			return pygame.font.SysFont(name, utilities.scaleH(size), bold, italic)
-            logsupport.Logs.Log('Generated Font: ', repr(name), str(size), str(utilities.scaleH(size)), str(bold),
-                                str(italic))
+
 		size = int(fsize)
 		try:
 			return self.fontcache[face][size][italic][bold]
 		except KeyError:
 			name = face if face != "" else None
 			if face not in self.fontcache:
-                self.fontcache[face] = {size: {italic: {bold: gennewfont(name, size, bold, italic)}}}
+				self.fontcache[face] = {size: {italic: {bold: gennewfont(name, size, bold, italic)}}}
 			else:
 				if size not in self.fontcache[face]:
-                    self.fontcache[face][size] = {italic: {bold: gennewfont(name, size, bold, italic)}}
+					self.fontcache[face][size] = {italic: {bold: gennewfont(name, size, bold, italic)}}
 				else:
 					if italic not in self.fontcache[face][size]:
-                        self.fontcache[face][size][italic] = {bold: gennewfont(name, size, bold, italic)}
+						self.fontcache[face][size][italic] = {bold: gennewfont(name, size, bold, italic)}
 					else:
 						if bold not in self.fontcache[face][size][italic]:
-                            self.fontcache[face][size][italic][bold] = gennewfont(name, size, bold, italic)
+							self.fontcache[face][size][italic][bold] = gennewfont(name, size, bold, italic)
 						else:
 							pass  # log this - should never get here
 			debug.debugPrint('Fonts', 'New font: ', face if face != "" else '-Sys-', ' :', size, (' b', ' B')[bold],
