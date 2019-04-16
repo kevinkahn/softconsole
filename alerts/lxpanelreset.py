852e97cdd80ca1abf876ed58e33e8e153e1bea16
kevinkahn <kevin@coral-zone.com>
2019-04-16 16:56:48 -0700

    Add lxpanelrestart alert to work around Raspbian memory leak

---
+++ alerts/lxpanelreset.py
@@ -0,0 +1,28 @@
+import alerttasks
+import logsupport
+import subprocess
+from logsupport import ConsoleWarning
+
+# noinspection PyUnusedLocal
+class LXPanelRestart(object):
+	def __init__(self):
+		pass
+
+	# @staticmethod
+	@staticmethod
+	def LXPanelRestart(alert):
+		try:
+			cmd = 'XAUTHORITY=/home/pi/.Xauthority DISPLAY=":{}.0" lxpanelctl restart'
+			with open('/dev/null') as null:
+				p0 = subprocess.call(cmd.format(0), shell=True)#, stdout=null, stderr=null)
+				p1 = subprocess.call(cmd.format(1), shell=True)#, stdout=null, stderr=null)
+			if (p0 != 0) or (p1 != 0):
+				logsupport.Logs.Log('LXPanel restart error {} {}'.format(p0,p1),severity=ConsoleWarning)
+			else:
+				logsupport.Logs.Log('LXPanels were restarted')
+		except Exception as E:
+			logsupport.Logs.Log('Exception in lxpanelrestart: {}'.format(repr(E)))
+
+
+
+alerttasks.alertprocs["LXPanelRestart"] = LXPanelRestart
