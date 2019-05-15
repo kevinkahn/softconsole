e03edf1d5fa65a856ed993e0e35169d79ddff1b3
Kevin Kahn <kevin@coral-zone.com>
2019-05-14 19:59:37 -0700

    Threshhold

--- controlevents.py
+++ controlevents.py
@@ -16,7 +16,7 @@ CEvent = Enum('ConsoleEvent',
 ConsoleOpsQueue = queue.Queue()  # master sequencer
 
 latencynotification = 1000 # notify if a loop latency is greater than this
-LateTolerance = 1  # for my systems
+LateTolerance = 1.5  # for my systems
 queuedepthmax = 0
 queuetimemax = 0
 
