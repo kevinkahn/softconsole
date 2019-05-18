fb89de4a5c89ed39d3e91a2305154231584c8ad2
Kevin Kahn <kevin@coral-zone.com>
2019-05-18 10:22:57 -0700

    Make sha check for autoversion async

--- alerts/autoversion.py
+++ alerts/autoversion.py
@@ -6,14 +6,37 @@ import exitutils
 import githubutil
 import historybuffer
 import logsupport
-import timers
 import threading
 from logsupport import ConsoleWarning, ConsoleDetail, ReportStatus
 import controlevents
 
 
 def DoFetchRestart():
-	logsupport.Logs.Log('Update fetch thread started')
+	global fetcher
+	try:  # if network is down or other error occurs just skip for now rather than blow up todo move sha get to thread
+		historybuffer.HBNet.Entry('Autoversion get sha')
+		sha, c = githubutil.GetSHA(config.sysStore.versionname)
+		historybuffer.HBNet.Entry('Autoversion get sha done')
+		# logsupport.Logs.Log('sha: ',sha, ' cvshha: ',config.versionsha,severity=ConsoleDetail)
+		if sha != config.sysStore.versionsha and sha != 'no current sha':
+			logsupport.Logs.Log('Current hub version different')
+			logsupport.Logs.Log(
+				'Running (' + config.sysStore.versionname + '): ' + config.sysStore.versionsha + ' of ' + config.sysStore.versioncommit)
+			logsupport.Logs.Log('Getting: ' + sha + ' of ' + c)
+		elif sha == 'no current sha':
+			logsupport.Logs.Log('No sha for autoversion: ', config.sysStore.versionname, severity=ConsoleWarning)
+			fetcher = None  # allow next autoversion to proceed
+			return
+		else:
+			fetcher = None  # allow next autoversion to proceed
+			return
+	except:
+		logsupport.Logs.Log(
+			'Github check not available' + str(sys.exc_info()[0]) + ': ' + str(sys.exc_info()[1]),
+			severity=ConsoleWarning)
+		fetcher = None  # allow next autoversion to proceed
+		return
+	logsupport.Logs.Log('Update fetch started')
 	ReportStatus("auto updt firmware", hold = 1)
 	githubutil.StageVersion(config.sysStore.ExecDir, config.sysStore.versionname, 'Auto Dnld')
 	logsupport.Logs.Log('Update fetch thread staged')
@@ -35,7 +58,7 @@ fetcher = None
 # noinspection PyUnusedLocal
 class AutoVersion(object):
 	def __init__(self):
-		fetchrestartinprogress = False
+		global fetcher
 		fetcher = None
 
 	# @staticmethod
@@ -44,32 +67,15 @@ class AutoVersion(object):
 		global fetcher
 		if config.sysStore.versionname not in ('none', 'development'):  # skip if we don't know what is running
 			logsupport.Logs.Log("Autoversion found named version running: ", config.sysStore.versionname, severity=ConsoleDetail)
+			if fetcher is not None and not fetcher.is_alive():
+				logsupport.Logs.Log('Autoversion fetcher found unexpectedly dead - retrying', severity=ConsoleWarning)
+				fetcher = None
 			# noinspection PyBroadException
-			try:  # if network is down or other error occurs just skip for now rather than blow up todo move sha get to thread
-				historybuffer.HBNet.Entry('Autoversion get sha')
-				sha, c = githubutil.GetSHA(config.sysStore.versionname)
-				historybuffer.HBNet.Entry('Autoversion get sha done')
-				# logsupport.Logs.Log('sha: ',sha, ' cvshha: ',config.versionsha,severity=ConsoleDetail)
-				if sha != config.sysStore.versionsha and sha != 'no current sha':
-					logsupport.Logs.Log('Current hub version different')
-					logsupport.Logs.Log(
-						'Running (' + config.sysStore.versionname + '): ' + config.sysStore.versionsha + ' of ' + config.sysStore.versioncommit)
-					logsupport.Logs.Log('Getting: ' + sha + ' of ' + c)
-					if fetcher is None:
-						fetcher = threading.Thread(name='AutoVersionFetch', target=DoFetchRestart, daemon=True)
-						fetcher.start()
-					else:
-						logsupport.Logs.Log('Autoversion fetch while previous fetch still in progress')
-				elif sha == 'no current sha':
-					logsupport.Logs.Log('No sha for autoversion: ', config.sysStore.versionname, severity=ConsoleWarning)
-				else:
-					pass
-			# logsupport.Logs.Log('sha equal ',sha,severity=ConsoleDetail)
-
-			except:
-				logsupport.Logs.Log(
-						'Github check not available' + str(sys.exc_info()[0]) + ': ' + str(sys.exc_info()[1]),
-						severity=ConsoleWarning)
+			if fetcher is None:
+				fetcher = threading.Thread(name='AutoVersionFetch', target=DoFetchRestart, daemon=True)
+				fetcher.start()
+			else:
+				logsupport.Logs.Log('Autoversion fetch while previous fetch still in progress')
 		else:
 			logsupport.Logs.Log("Auto version found special version running: ", config.sysStore.versionname)
 
