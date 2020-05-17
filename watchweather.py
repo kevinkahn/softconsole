9fc27a8a3f0cfefb779d118828bd908b617634f6
Kevin Kahn <kevin@coral-zone.com>
2020-05-17 10:16:45 -0700

    Update tracker tool

--- watchweather.py
+++ watchweather.py
@@ -16,38 +16,44 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
 	global statdump, NodeTally, LocationTally, LocLastFetch, LocNames
-	topic = msg.topic.split('/')[4]
-	payload = msg.payload.decode('ascii')
-	p = json.loads(payload)
-	loc = p['location']
-	ft = p['fetchtime']
-	fn = p['fetchingnode']
-	if topic in LocationTally:
-		LocationTally[loc] += 1
-		gap = ft - LocLastFetch[loc]
-		LocLastFetch[loc] = ft
-	else:
-		LocNames[loc] = topic
-		LocationTally[loc] = 1
-		LocLastFetch[loc] = ft
-		gap = -1
-	if fn in NodeTally:
-		NodeTally[fn] += 1
-	else:
-		NodeTally[fn] = 1
-	if time.time() - statdump > 3600:
-		statdump = time.time()
-		with open('tally', 'a') as f:
-			f.write('----- Nodes --------\n')
-			for n in NodeTally:
-				f.write('{}: {}\n'.format(n, NodeTally[n]))
-			f.write('----- Locations --------\n')
-			for l in LocationTally:
-				f.write('{}: {}    ({})\n'.format(LocNames[l], LocationTally[l], l))
+	try:
+		topic = msg.topic.split('/')[4]
+		payload = msg.payload.decode('ascii')
+		p = json.loads(payload)
+		loc = p['location']
+		ft = p['fetchtime']
+		fn = p['fetchingnode']
+		if loc in LocationTally:
+			LocationTally[loc] += 1
+			gap = ft - LocLastFetch[loc]
+			LocLastFetch[loc] = ft
+		else:
+			LocNames[loc] = topic
+			LocationTally[loc] = 1
+			LocLastFetch[loc] = ft
+			gap = -1
+		if fn in NodeTally:
+			NodeTally[fn] += 1
+		else:
+			NodeTally[fn] = 1
+		if time.time() - statdump > 3600:
+			statdump = time.time()
+			with open('tally', 'a') as f:
+				f.write('----- Nodes --------\n')
+				for n in NodeTally:
+					f.write('{}: {}\n'.format(n, NodeTally[n]))
+				f.write('----- Locations --------\n')
+				for l in LocationTally:
+					f.write('{}: {}    ({})\n'.format(LocNames[l], LocationTally[l], l))
+			with open('watchw', 'a') as f:
+				f.write('------\n')
 
-	print(topic, fn, loc, p['fetchcount'], time.strftime('%H:%M:%S', time.localtime(ft)))
-	with open('watchw', 'a') as f:
-		f.write('{}, {}, {}, {}, {}, {}\n'.format(topic, fn, gap, loc, ft, p['fetchcount']))
+		print(topic, fn, loc, p['fetchcount'], time.strftime('%H:%M:%S', time.localtime(ft)), gap)
+		with open('watchw', 'a') as f:
+			f.write('{}, {}, {}, {}, {}, {}\n'.format(topic, fn, gap, loc, ft, p['fetchcount']))
+	except Exception as E:
+		print('Exception in loop for {} E: {}'.format(topic, E))
+		print(payload)
 
 
 MQTTclient = mqtt.Client()
