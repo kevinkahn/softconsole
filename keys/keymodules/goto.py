a9521302d6801a299ea0a5e1171983f50c98fa54
Kevin Kahn <kevin@coral-zone.com>
2020-11-20 17:12:14 -0800

    Factor keys

---
+++ keys/keymodules/goto.py
@@ -0,0 +1,32 @@
+import debug
+import logsupport
+import screen
+from configobjects import GoToTargetList
+from guicore.switcher import SwitchScreen
+from logsupport import ConsoleWarning
+from toucharea import ManualKeyDesc
+from keys.keyspecs import KeyTypes
+
+
+class GoToKey(ManualKeyDesc):
+	# Note: cannot do verify on a goto key - would make no sense of the verify pop back to previous screen
+	# this is actually a Push Screen key in that it makes a stack entry
+	def __init__(self, thisscreen, keysection, keyname):
+		debug.debugPrint('Screen', "             New GoTo Key ", keyname)
+		# todo remove any Verify from the keysection
+		ManualKeyDesc.__init__(self, thisscreen, keysection, keyname)
+		screen.AddUndefaultedParams(self, keysection, ScreenName='**unspecified**')
+		self.targetscreen = None
+		GoToTargetList[self] = self.ScreenName
+
+		self.Proc = self.GoToKeyPressed
+
+	def GoToKeyPressed(self):
+		if self.targetscreen is None:
+			logsupport.Logs.Log('Unbound GOTO key {} pressed'.format(self.name), severity=ConsoleWarning)
+			self.ScheduleBlinkKey(20)
+		else:
+			SwitchScreen(self.targetscreen, 'Bright', 'Direct go to' + self.targetscreen.name, push=True)
+
+
+KeyTypes['GOTO'] = GoToKey
