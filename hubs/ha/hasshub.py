b748e218dac8baef69514f142e52a71fe3495cd4
kevinkahn <kevin@coral-zone.com>
2022-01-18 14:06:13 -0800

    Add ignore scene_reloaded event

--- hubs/ha/hasshub.py
+++ hubs/ha/hasshub.py
@@ -30,7 +30,7 @@ ignoredeventtypes = [
 	'hacs/repository', 'hacs/config', 'entity_registry_updated', 'component_loaded', 'device_registry_updated',
 	'entity_registry_updated', 'lovelace_updated', 'isy994_control', 'core_config_updated', 'homeassistant_start',
 	'config_entry_discovered', 'automation_reloaded', 'hacs/stage', 'hacs/reload', 'zwave_js_value_notification',
-	'event_template_reloaded', 'panels_updated', 'data_entry_flow_progressed']
+	'event_template_reloaded', 'panels_updated', 'data_entry_flow_progressed', 'scene_reloaded']
 
 
 def stringtonumeric(v):
