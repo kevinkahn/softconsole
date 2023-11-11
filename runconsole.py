bfca8b3a7678383372a74f784353caca2b08018b
Kevin Kahn <kevinkahn@users.noreply.github.com>
2023-11-11 10:45:39 -0800

    Try to brute force hang issue from runconsole

--- runconsole.py
+++ runconsole.py
@@ -6,19 +6,41 @@ and selects the version of console to actually use.
 For now it looks for a file usebeta to select the beta version.  If a file versionselector exists that is used instead after prefixing with "console".
 This might make some testing scenarios easier
 """
+from multiprocessing import set_start_method
+
+set_start_method("spawn")
 
 import os
+import time
 import subprocess
+import signal
 
 if os.path.isfile('versionselector'):
 	with open('versionselector', 'r') as f:
 		vers = f.readline().rstrip('\n')
 	versdir = 'console' + vers
+else:
+	print('No Version Set!')
+	exit(15)  # don't have systemd do a restart
 
 print('Starting using directory: ' + versdir)
 os.chdir(versdir)
+if os.path.exists('../running'):
+	os.remove('../running')
 
-consolepid = subprocess.Popen(['python3', 'console.py']).pid
+while True:
+	consoleproc = subprocess.Popen(['python3', 'console.py'])
+	consolepid = consoleproc.pid
+	time.sleep(10)
+	if os.path.exists('../running'):
+		print('Console running with pid: {}'.format(consolepid))
+		break
+	print('Console start hung - retry')
+	consoleproc.send_signal(signal.SIGINT)
+	print('Semt signal')
+	while consoleproc.poll() is None:
+		print('Waiting termination')
+		time.sleep(1)
 
 with open('../console.pid', 'w') as f:
 	f.write(str(consolepid))
