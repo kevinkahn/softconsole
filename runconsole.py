3edf8a53ede9f838120bd99283d1d8cee820b5df
kevinkahn <kevin@coral-zone.com>
2023-11-11 10:15:19 -0800

    Trying brute force to get past the hang

--- runconsole.py
+++ runconsole.py
@@ -8,17 +8,36 @@ This might make some testing scenarios easier
 """
 
 import os
+import time
 import subprocess
+import signal
 
 if os.path.isfile('versionselector'):
 	with open('versionselector', 'r') as f:
 		vers = f.readline().rstrip('\n')
 	versdir = 'console' + vers
+else:
+	print('No Version Set!')
+	exit(15)  # don't have systemd do a restart
 
 print('Starting using directory: ' + versdir)
 os.chdir(versdir)
+if os.path.exists('../running'):
+	os.remove('../running')
 
-consolepid = subprocess.Popen(['python3', 'console.py']).pid
+while True:
+	consoleproc = subprocess.Popen(['python3', 'console.py'])
+	consolepid = consoleproc.pid
+	time.sleep(10)
+	if os.path.exists('../running'):
+		print('Console running with pid: {}'.format(consolepid))
+		break
+	print('Console start hung - retry')
+	consoleproc.send_signal(signal.SIGINT)
+	print('Semt signal')
+	while consoleproc.poll() is None:
+		print('Waiting termination')
+		time.sleep(1)
 
 with open('../console.pid', 'w') as f:
 	f.write(str(consolepid))
